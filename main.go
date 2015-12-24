e5160421eabcbe717c8cbc27dc91c2a8c3243f2e
Otto Giron <otto.giron@xoom.com>
2015-12-23 23:31:03 -0600

    First commit code example

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/ottogiron/chapi/server"
+)
+
+type HelloController struct {
+	*server.BasePlugin
+}
+
+func (c *HelloController) Register(s server.Server) {
+	s.HandleFunc("/", handleHello).Methods("GET")
+}
+
+func handleHello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "!!!Hello Chapi")
+}
+
+func main() {
+
+	connectionString := ":8000"
+	s := server.NewServer()
+	s.Register(&HelloController{})
+	err := s.Run(connectionString)
+	if err != nil {
+		fmt.Println("Error when running server", err)
+	}
+
+}
